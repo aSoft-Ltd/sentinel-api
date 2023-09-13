6d16286b5438f9edac26e900fbb362701723b5c1
andylamax <andylamax@programmer.net>
2023-09-13 05:30:12 +0300

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,29 @@
+import java.io.File
+
+pluginManagement {
+    includeBuild("../build-logic")
+}
+
+plugins {
+    id("multimodule")
+}
+
+fun includeSubs(base: String, path: String = base, vararg subs: String) {
+    subs.forEach {
+        include(":$base-$it")
+        project(":$base-$it").projectDir = File("$path/$it")
+    }
+}
+
+listOf(
+    "cinematic", "keep", "lexi", "neat", "kash", "geo", "kase",
+    "kronecker", "symphony", "epsilon", "krono", "hormone", "identifier",
+    "kommerce", "kollections", "koncurrent", "kommander", "cabinet-api", "snitch"
+).forEach { includeBuild("../$it") }
+
+rootProject.name = "sentinel"
+
+includeSubs(base = "sentinel-reception-api", path = "../sentinel/reception", "core")
+includeSubs(base = "sentinel-registration-api", path = "../sentinel/registration", "core")
+includeSubs(base = "sentinel-enterprise-authentication-api", path = "../sentinel/enterprise/authentication", "core")
+includeSubs(base = "sentinel-enterprise-profile-api", path = "../sentinel/enterprise/profile", "core")
